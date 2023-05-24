f16c171a793773e9f124520340f9992bcb1dc707
Rajkaran <rajkaranvir456@gmail.com>
2023-05-25 02:04:02 +0530

    🆕 NEW : addded new post - How to get started with programming

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Rajkaran Mishra'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "Rajkaran's Blog"
 
 export default function Layout({ children, home }) {
   return (
